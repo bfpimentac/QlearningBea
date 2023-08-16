1a8d1925b9c19aec7dc4506fb0ad038fd01ba9c7
lucashnss <hns.lucass@gmail.com>
2023-08-15 23:13:49 -0300

    versão enviada ao classroom

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import numpy as np
 
 s = cn.connect(2037)
 
-# escolha aleatória de ações
+# mapeamento de número para ação
 def conversionChoice(num):
     print(num)
     if num==0:
@@ -13,9 +13,6 @@ def conversionChoice(num):
         return "right"
     elif num==2:
         return "jump"
-
-# state = (plataforma, direcao)
-
 
 # conversão da plataforma e da direção de binários para decimal
 def conversion(state):
@@ -75,51 +72,60 @@ def q_update(q_table,state,action,next_state,rw,alpha,gamma):
     return q_value
 
 
-
+# uso da biblioteca numpy para ler o 'resultado.txt' como uma matriz
 q_table = np.loadtxt('resultado.txt')
 
+# setando as casas decimais dos valores da q_table
 np.set_printoptions(precision=6)
 
-state = (2,0)   # estado inicial 
+# estado inicial 
+state = (0,0)   
 
 alpha = 0.1     # taxa de aprendizagem que diz o quão rápido o agente aprende
 gamma = 0.8     # fator de desconto, diz o peso da recompensa futura em relação à imediata
 epsilon = 0    # epsilon greedy strategy -> uma taxa que define se o agente irá tomar ações aleatórias ou embasadas
 
 while(True):
+    # gera um número aleatório
+    random_num =  random.randint(0,2)                       
+    # transforma para a ação (aleatória)
+    random_action = conversionChoice(random_num)            
+    # recebe o index da linha do estado atual
+    state_index = get_state(state[0],state[1])              
+    # gera a melhor ação para o estado atual
+    based_num = best_action(state_index,q_table)            
+    # transforma um número para a ação 
+    based_action = conversionChoice(based_num)              
+    # gera um número aleatório entre 0 e 1 - excluindo 1
+    random_float = random.uniform(0,1)                    
 
-    random_num =  random.randint(0,2)                       # gera um número aleatório
-
-    random_action = conversionChoice(random_num)            # transforma para a ação (aleatória)
-
-    state_index = get_state(state[0],state[1])              # recebe o index da linha do estado
-
-    based_num = best_action(state_index,q_table)            # gera a melhor ação para o estado atual
-
-    based_action = conversionChoice(based_num)              # transforma um número para a ação 
-
-    random_float = random.uniform(0,1)                    # gera um número aleatório entre 0 e 1 - excluindo 1
-
-    if (random_float >= epsilon):                           # caso o valor gerado seja maior que epsilon o agente 
-        action_num = based_num                              # irá usar a q_table para tomar a ação, caso     
-        action = based_action                               # contrário será aleatória sua tomada de decisão
+    # caso o valor gerado seja maior que epsilon o agente irá usar a q_table para tomar a ação, caso contrário será aleatória sua tomada de decisão   
+      
+    if (random_float >= epsilon):                          
+        action_num = based_num                              
+        action = based_action                               
     else:                                                   # epsilon = 0 -> ações sempre usando a q_table    
         action_num = random_num                             # epsilon = 1 -> ações sempre aleatórias
         action = random_action  
 
-    next_state, rw = cn.get_state_reward(s, action)          # chamada da função para ativar a ação e receber o estado / recompensa
+    # chamada da função para ativar a ação e receber o estado / recompensa
+    next_state, rw = cn.get_state_reward(s, action)          
 
+    # prints a título apenas de ter uma ideia do funcionamento do robô enquanto ele opera
     print(f'action:{action}')
     print(f'state:{next_state}')
     print(f'bounty:{rw}') 
 
-    next_state = conversion(next_state)                       # converte o estado binário dado em uma tupla com a plataforma e a direção em decimais
+    # converte o estado binário dado em uma tupla com a plataforma e a direção em decimais
+    next_state = conversion(next_state)                       
+     # recebe o index da linha do estado
+    next_state_index = get_state(next_state[0],next_state[1]) 
 
-    next_state_index = get_state(next_state[0],next_state[1]) # recebe o index da linha do estado
+    # atualiza a q_table
+    q_table[state_index, action_num] = q_update(q_table,state_index,action_num,next_state_index,rw,alpha,gamma)   
 
-    q_table[state_index, action_num] = q_update(q_table,state_index,action_num,next_state_index,rw,alpha,gamma)   # atualiza a q_table
+    # escreve no resultado.txt
+    np.savetxt('resultado.txt', q_table, fmt="%f")             
 
-    np.savetxt('resultado.txt', q_table, fmt="%f")             # escreve no resultado.txt
-
-
-    state = next_state                                         # o estado atual era o antigo próximo estado
+    # o estado atual era o antigo próximo estado
+    state = next_state                                         
